b7eddfa720651e68af4debc4d11803c65e4c5a06
Timothy Aurelio Cannavaro <89766992+varomnrg@users.noreply.github.com>
2023-10-31 05:57:28 +0000

    Added refresh token

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,5 +3,6 @@ const authControllers = require("../controllers/auth");
 
 router.post("/register", authControllers.register);
 router.post("/login", authControllers.login);
+router.post("/refreshtoken", authControllers.refreshToken);
 
 module.exports = router;
